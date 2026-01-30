8aa9ba797bf4287e0d85235dfec5f49ced313e2a
bhamareketan16-glitch <bhamareketan16@gmail.com>
2026-01-30 15:22:42 +0530

    Update script.js

--- semester-2/batch-2/lec-6/script.js
+++ semester-2/batch-2/lec-6/script.js
@@ -1,105 +1,81 @@
-// const cart = ["shirt", "trousers", "shoes", "shades"];
+const cart = ["shirt", "trousers", "shoes", "shades"]
 
-// const promise = createOrder(cart); // An async function -- will get orderId
-
-// //Print PROMISE
-// console.log(promise);
-
-// //Consume
-// promise.then(function (orderId) {
-//     console.log("inside then", promise);
-//   //This runs when Promise is resolved
-//   console.log(orderId);
-// }).catch(function(err){
-//     console.log(err.message)
-//     alert("error",err.message)
-// })
-
-// //Creation of promise/ backend side / producer side
-
-// function createOrder(cart) {
-
-// function validateCart() {
-//   return false;
-// }
-
-//   const pr = new Promise(function (resolve, reject) {
-//     //createOrder
-//     //validate the cart (check in stock)
-//     //return orderId
-
-//     //Fail-first approach
-//     if (!validateCart(cart)) {
-//       setTimeout(function () {
-//         // mimicking async BE jobs.
-//         const err = new Error("Cart is invalid");
-//         reject(err);
-//       }, 2000);
-//     }
-
-//     // logic for createOrder
-
-//     const orderId = "11010";
-
-//     if (orderId) {
-//       setTimeout(function () {
-//         // mimicking BE jobs.
-//         resolve(orderId);
-//       }, 5000);
-//     }
-//   });
-
-//   return pr;
-// }
-
-//======================= Promise chaining=======================
-const cart = ["shirt", "trousers", "shoes", "shades"];
+console.log("cart items", cart)
 
 createOrder(cart)
   .then(function (orderId) {
-    console.log(orderId);
-    return orderId;
+    console.log("order created")
+    console.log("order id is", orderId)
+
+    return orderId
   })
   .then(function (orderId) {
-    return proceedToPayment(orderId);
-  }).then((paymentInfo)=> {
-        console.log(paymentInfo)
-    })
+    console.log("going for payment with order id", orderId)
+
+    return proceedToPayment(orderId)
+  })
+  .then(function (paymentInfo) {
+    console.log("payment response received")
+    console.log(paymentInfo)
+
+    return paymentInfo
+  })
   .catch(function (err) {
-    console.log(err.message);
-  }).then(function(){
-    console.log("I will be definitely called !!")
-  });
+    console.log("error happened")
+    console.log(err.message)
+  })
+  .then(function () {
+    console.log("this will always execute")
+  })
 
-// Creation / Backend side /Promises are produced
+
+
 function createOrder(cart) {
-  const pr = new Promise(function (resolve, reject) {
-    //createOrder
-    //validateCart
-    //return orderId
 
-    //Fail-first approach
+  function validateCart(cart) {
+    return false
+  }
+
+  console.log("creating order...")
+
+  const promise = new Promise(function (resolve, reject) {
+
+    console.log("inside promise")
+
     if (!validateCart(cart)) {
-      const err = new Error("Cart is invalid");
-      reject(err);
+
+      console.log("cart validation failed")
+
+      setTimeout(function () {
+        reject(new Error("Cart is invalid"))
+      }, 2000)
+
+      return
     }
 
-    // logic for createOrder
+    console.log("cart validated")
 
-    const orderId = "11011";
-    if (orderId) {
-      setTimeout(() => resolve(orderId), 4000);
-    }
-  });
-  return pr;
+    const orderId = "11011"
+
+    setTimeout(function () {
+      console.log("order created successfully")
+      resolve(orderId)
+    }, 4000)
+  })
+
+  return promise
 }
+
+
 
 function proceedToPayment(orderId) {
-  return new Promise(function (resolve, reject) {
-    resolve("Payment Successful");
-  });
-}
+  console.log("payment started for", orderId)
 
-function validateCart(cart) {
-  return false;
+  return new Promise(function (resolve, reject) {
+
+    setTimeout(function () {
+      console.log("payment processing...")
+      resolve("Payment Successful")
+    }, 3000)
+  })
 }
