eb1308d2e72eb101657f473d7fd89a20487025de
bhamareketan16-glitch <bhamareketan16@gmail.com>
2026-01-30 15:17:14 +0530

    Refactor string.js to improve readability and structure

--- semester-2/batch-2/lec-3/string.js
+++ semester-2/batch-2/lec-3/string.js
@@ -1,50 +1,27 @@
-let str = "   Hello Developers cat dog  "; //0 based indexing.
-
+let str = "   Hello Developers cat dog  "; 
 console.log(str)
-console.log(str.length) // length
-// 
-console.log(str.charAt(9));    //char at given index
+console.log(str.length)
 
-console.log(str.charCodeAt(9));   //ASCII code at given index
+console.log(str.charAt(9));
+console.log(str.charCodeAt(9));  
 
-//Substring
+console.log(str.substring(10)); 
 
-console.log(str.substring(10)); // startIdx only will print until the end.
-
-//start < end
-console.log(str.substring(10,12)); //str[10],str[11]
-
+console.log(str.substring(10,12));
 //start > end
-console.log(str.substring(12,10)); //str[10],str[11] //behave as str.substring(10,12)
+console.log(str.substring(12,10));
 
-//-ve index. converted to 0 by default.
-
-console.log(str.substring(-100)); // converts to str.substring(0)
-
-
-//Slice
+console.log(str.substring(-100)); 
 
 let sl1 = str.slice(10); 
-console.log(sl1); // starting on 10 idx print until the end.
-
-
-//start < end
-
+console.log(sl1); 
 let sl2 = str.slice(10,12);
-console.log(sl2); //str[10],str[11]
+console.log(sl2); 
 
-
-//start > end
-let sl3 = str.slice(12,10); //empty string
+let sl3 = str.slice(12,10); 
 console.log(sl3); 
-
-//-ve index. -> sliced from the end. Get the string from the last index.
-
 let sl4 = str.slice(-10);
 console.log(sl4);
-
-
-
 console.log(str.toLowerCase());
 console.log(str.toUpperCase());
 console.log(str.trim());
